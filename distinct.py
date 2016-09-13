1604b36662bce74b604dc79c2ad1d6073ee62ca2
elakamarcus <n0m4d@hotmail.com>
2016-09-13 13:45:19 +0800

    Create distinct.py
    Show the number of distinct terms for i^j

---
+++ distinct.py
@@ -0,0 +1,7 @@
+q = {}
+q = set(q)
+for a in range (50,1000,50):
+  for i in range (2,1+a,1):
+     for j in range (2,1+a,1):
+        q.add(i**j)
+  print("(2 -> {})^(2 -> {}): {}".format(i,j,len(q)))
