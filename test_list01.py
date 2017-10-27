d56e4815ab5d2888142c71850c0e98c62242431c
Marcus Lauren Andersson <anderssonm@ai.pri>
2017-10-27 22:37:05 +0800

    update

--- test_list01.py
+++
@@ -1,6 +0,0 @@
-def sendTCP(d, p):
-    flags = ["S","A","F","R"]
-    for x in range (0, len(flags)):
-        print("IP(dst="+d+")/TCP(flags="+flags[x]+", sport=RandShort(), dport="+p)
-
-sendTCP("1.1.1.2", "2.2.2.1")
