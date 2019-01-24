c655cde818aabb663a3448791d41b30365ce7eab
elakamarcus <elakamarcus@users.noreply.github.com>
2019-01-24 11:46:20 +0800

    Create yl_aqi.py

---
+++ yl_aqi.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import lxml
+from unidecode import unidecode
+
+"""
+ A simple user-agent, for the request header
+"""
+rh = {'User-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0'}
+
+url = "https://aqicn.org/city/hongkong/yuen-long/"
+r = requests.get(url, headers = rh)
+soup = BeautifulSoup(r.text, "lxml")
+AQID = soup.find("div", property="id=aqiwgtvalue")
+x = soup.find("div", {"class":"aqivalue"})
+
+print "[+] " + x.text
+print "[!] " + x["title"].encode('ascii', 'ignore')
