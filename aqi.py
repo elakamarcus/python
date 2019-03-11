7919784fd6f5bdc89d860cd7d76f1c49666405b3
elakamarcus <elakamarcus@users.noreply.github.com>
2019-03-11 17:34:15 +0800

    Create aqi.py

---
+++ aqi.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+import lxml
+from unidecode import unidecode
+
+rh = {'User-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0'}
+
+yuenlong = "https://aqicn.org/city/hongkong/yuen-long/"
+
+urllist = ["https://aqicn.org/city/hongkong/yuen-long/","https://aqicn.org/city/sweden/stockholm-sodermalm/","http://aqicn.org/city/jiangsu/jintan/shijiancezhan/","http://aqicn.org/city/jiangsu/changzhou/"]
+city = ["Yuen Long", "Stockholm", "Jintan", "Changzhou"]
+
+for a in range(0,4):
+        url = urllist[a]
+        r = requests.get(url, headers = rh)
+        soup = BeautifulSoup(r.text, "lxml")
+        x = soup.find("div", {"class":"aqivalue"})
+        print "[+] " + city[a] + ": " + x.text + " [!] " + x["title"].encode('ascii', 'ignore')
+
