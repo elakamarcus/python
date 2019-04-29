2e606cb09a64029c6d8c7d26037978e8a59df229
elakamarcus <elakamarcus@users.noreply.github.com>
2019-04-29 22:42:29 +0800

    Update hsbc_hibor.py
    Added prime

--- hsbc_hibor.py
+++ hsbc_hibor.py
@@ -1,4 +1,5 @@
-## HSBC HK Hibor rate
+## HSBC HK Hibor scraping...
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -9,19 +10,40 @@ def countRate(r):
         return 2.375
     return r+x
 
+def primeRate(r):
+#x is to be set to the variable...
+    x = 2.750
+    return r-x
+
+
 # UA
 bh = {"User-agent" : "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0"}
 
 # URL to table details
-url = "https://rbwm-api.hsbc.com.hk/digital-pws-tools-hibor-eapi-prod-proxy/v1/hibor?year="
-q = [0]
-r = requests.get(url, headers = bh)
-for a in r.json()["rateTable"][0].values():
-    q.append(a)
+urla = "https://rbwm-api.hsbc.com.hk/digital-pws-tools-hibor-eapi-prod-proxy/v1/hibor?year="
+qa = [0]
+ra = requests.get(urla, headers = bh)
+for a in ra.json()["rateTable"][0].values():
+    qa.append(a)
 
-dly = countRate(float(q[2]))
+dly = countRate(float(qa[2]))
 
-print("Date: {}\nHibor: {:.3f} + 1.300% = {}\nMax Rate: {}%".format(q[1], float(q[2]), float(dly)+1.300,dly))
+print("-----HIBOR Rate-----")
 
-## TODO:
-## get the prime rate as well, P+y
+print("Date: {}\nHibor: {:.3f} + 1.300 = {}\nRate: {}%\n".format(qa[1], float(qa[2]), float(dly)+1.300,dly))
+
+print("-----Prime Rate-----")
+
+qb = [0]
+pb = [0]
+urlb = "https://www.hsbc.com.hk/investments/market-information/hk/lending-rate/"
+rb = requests.get(urlb, headers = bh)
+
+soup = BeautifulSoup(rb.text, 'lxml')
+for th in soup.findAll("th"):
+    qb.append(th.get_text())
+
+for ok in soup.findAll("td"):
+    pb.append(ok.get_text())
+
+print("HSBC Prime Rate.\nLast set: {}\nPrime: {}- 2.750%\nRate: {}%".format(qb[3], pb[1], primeRate(float(pb[1].replace(" ", "").replace("%","")))))
