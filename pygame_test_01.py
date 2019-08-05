f9aea59ee3906c8c2ba8d0f2bb23777dfe1cdcab
elakamarcus <n0m4d@hotmail.com>
2019-08-05 23:28:31 +0800

    ~

--- pygame_test_01.py
+++ pygame_test_01.py
@@ -23,25 +23,33 @@ def vaultBoy(x,y):
 x = (width * 0.5)
 y = (height * 0.5)
 x_change = 0
-car_speed = 0
+y_change = 0
+pic_speed = 0
 
 while not crashed:
+    # todo: use speed instead of x y.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed == True
-        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                y_change = -5
+            elif event.key == pygame.K_DOWN:
+                y_change = 5
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 x_change = -5
             elif event.key == pygame.K_RIGHT:
                 x_change = 5
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 x_change = 0
+                y_change = 0
 
+    y += y_change
     x += x_change
     # kill the game if reach either side
-    if(x==800 or x==0):
+    if(x==800 or x==0 or y==600 or y==0):
         pygame.quit()
 
     gameDisplay.fill(white)
